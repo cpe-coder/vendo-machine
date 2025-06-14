338c52337f40d48f34f31c4601a2aa328657c2b4
Grian Gajila <griangajila2@gmail.com>
2025-06-14 11:49:38 +0800

    wip

--- vendo-machine.ino
+++ vendo-machine.ino
@@ -12,12 +12,13 @@ Servo servo2;
 
 #define capPin D3             
 #define trigPin D4            
-#define echoPin D5           
+#define echoPin D5          
+#define buzzer D0 
 
-#define WIFI_SSID ""
-#define WIFI_PASSWORD ""
-#define API_KEY ""
-#define DATABASE_URL ""
+#define WIFI_SSID "reyn!"
+#define WIFI_PASSWORD "12345678"
+#define API_KEY "AIzaSyASRThAyEla6vcjwCVm5yUZWG_72o5js84"
+#define DATABASE_URL "vendo-machine-94bd3-default-rtdb.firebaseio.com/"
 
 FirebaseData fbdo;
 FirebaseAuth auth;
@@ -29,7 +30,7 @@ bool plasticHandled = false;
 
 void setup() {
   Serial.begin(9600);
-  
+  pinMode(buzzer, HIGH);
   pinMode(capPin, INPUT);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
@@ -61,6 +62,7 @@ void loop() {
     sendDataPrevMillis = millis();
 
     int capValue = digitalRead(capPin); 
+
     long distance = getUltrasonic();
 
     Serial.print("Capacitive: ");
@@ -71,19 +73,25 @@ void loop() {
     if (capValue == 0) {
       Serial.println("Solid detected");
       Firebase.RTDB.setInt(&fbdo, "Detection/isBottle", false);
-      servo1.write(180);
-      delay(2000);
-      servo1.write(0);
+      digitalWrite(buzzer, HIGH);
+      delay(3000);
+      digitalWrite(buzzer, LOW);
       plasticHandled = false;  
-    } else if (capValue == 1 && distance < 5 && !plasticHandled) {
+    } else if (capValue == 1 && distance < 4 && !plasticHandled) {
       Serial.println("Plastic bottle detected");
       Firebase.RTDB.setInt(&fbdo, "Detection/isBottle", true);
-      servo2.write(180);
+      servo1.write(0);
+      delay(2000);
+      servo1.write(180);
+      servo2.write(90);
       plasticHandled = true; 
       delay(2000);
       Firebase.RTDB.setInt(&fbdo, "Detection/isBottle", false);
-    } else if (plasticHandled && distance < 5) {
+    } else if (plasticHandled && distance < 4) {
       Firebase.RTDB.setInt(&fbdo, "Detection/isBottle", true);
+      servo1.write(0);
+      delay(2000);
+      servo1.write(180);
       servo2.write(0);
       plasticHandled = false;
       delay(2000);
@@ -94,6 +102,7 @@ void loop() {
 }
 
 long getUltrasonic() {
+
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
